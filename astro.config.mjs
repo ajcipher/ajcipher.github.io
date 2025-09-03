77021877eafbf1568d9ba6997f0ee20761318bd4
ajh4ck <armandoz_95@live.com>
2025-09-03 15:48:58 -0600

    Agrego proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/writeups-hacking.github.io/', // <-- añade esta línea
   vite: {
     plugins: [tailwindcss()]
   }
